c26ce2ccacf1cc9e1b2c8c673fc989e69e7b44ef
Josh <josh.mapley@traffic.com.au>
2022-04-07 16:48:53 +1000

    updates for css hydration to prevent brief unstyled flash

--- components/common/header/header.tsx
+++ components/common/header/header.tsx
@@ -43,7 +43,7 @@ const Header = ({ navigation }) => {
     setDrawerOpen(false);
   });
 
-  const isMobile = useMedia("(max-width: 768px)");
+  const isMobile = useMedia("(max-width: 768px)", false);
   const [drawerOpen, setDrawerOpen] = useState(false);
   return (
     <Wrapper ref={header} stuck={hasStuck}>
