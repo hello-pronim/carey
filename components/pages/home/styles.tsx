08bf41e9b44f4d41b268c13bb009526d2dd2fdf6
Arman Torosyan <arman.torosyan@tirsoft.co>
2022-06-03 15:49:43 +0400

    Accordion sets

--- components/pages/home/styles.tsx
+++ components/pages/home/styles.tsx
@@ -1,5 +1,17 @@
 import { styled } from "@styles/stitches";
 
 const Example = styled("div", {});
+const AccordionContainer = styled("div", {
+  width: "40%",
 
-export { Example };
+  "@media screen and (max-width: 768px)": {
+    width: "100%",
+  },
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
+  alignItems: "center",
+  paddingBottom: "40px",
+});
+
+export { Example, AccordionContainer };
