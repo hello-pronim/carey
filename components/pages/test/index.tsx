aa880860a9c3e2b5d5abe10267f94b5a3593dba0
Yaroslav Wen <hello.pronim@gmail.com>
2022-06-15 14:10:05 +1000

    fix: fix build issue in test page

--- components/pages/test/index.tsx
+++ components/pages/test/index.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { Image } from "@components/common";
 import { PreFooter } from "@components/common";
-import OpenMorningsCard from "@components/common/cards/openMornings/Card";
 
 import { Wrapper } from "./styles";
 
 const Test = () => {
   return (
     <Wrapper>
-      <OpenMorningsCard
-        title="Open Mornings"
-        description="We invite you to visit us to explore our school and discover why a Carey education is right for your family."
-        background="assets/img/open_mornings_img.png"
-        href="#"
-        size="large"
-      />
       <Image
         src="https://images.unsplash.com/photo-1629363447922-1f421b470828"
         alt="Image by Tim Schmidbauer on Unsplash"
