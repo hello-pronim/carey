989cb9e4bae322d979100f5a86a9e67f9032792c
Yaroslav Wen <hello.pronim@gmail.com>
2022-06-17 15:45:44 +1000

    draft: open mornings card(incompleted)

--- components/common/cards/openMornings/Card.tsx
+++ components/common/cards/openMornings/Card.tsx
@@ -1,23 +1,41 @@
 import React from "react";
-import { Button, Container } from "@components/common";
-import { ActionWrapper, Card, Description, Heading, Wrapper } from "./styles";
+import Image from "next/image";
+import { Button } from "@components/common";
+import MainImage from "public/assets/img/junior_img.png";
+import {
+  ActionWrapper,
+  Card,
+  CMarkImage,
+  Description,
+  DescriptionWrapper,
+  FooterWrapper,
+  Heading,
+  HeadingWrapper,
+  ImageWrapper,
+  Wrapper,
+} from "./styles";
 
 const OpenMorningsCard = ({
   title,
   description,
   href = "#",
-  image,
+  background,
+  type = "default",
   size = "small",
-}) => {
+}: CardPropsType) => {
   return (
-    <Card>
-      <Wrapper>
-        <Heading>{title}</Heading>
-        <Container innerCSS={{ mx: 0 }}>
-          <Description css={{ gridColumn: "1 / span 8" }}>
-            {description}
-          </Description>
-        </Container>
+    <Card
+      size={size}
+      type={type}
+      css={{ backgroundImage: `url(${background})` }}
+    >
+      <Wrapper size={size}>
+        <HeadingWrapper size={size}>
+          <Heading>{title}</Heading>
+          <DescriptionWrapper>
+            <Description size={size}>{description}</Description>
+          </DescriptionWrapper>
+        </HeadingWrapper>
         <ActionWrapper>
           <Button
             arrow
@@ -29,8 +47,33 @@ const OpenMorningsCard = ({
           />
         </ActionWrapper>
       </Wrapper>
+      <FooterWrapper size={size}>
+        <CMarkImage size={size} />
+        <ImageWrapper>
+          <Image
+            alt="main image"
+            src={MainImage}
+            width={480}
+            height={600}
+            layout="responsive"
+            objectFit="cover"
+            objectPosition="top"
+            priority
+          />
+        </ImageWrapper>
+      </FooterWrapper>
     </Card>
   );
+};
+
+type CardPropsType = {
+  title: string;
+  description: string;
+  background: string;
+  href?: string | Object;
+  type?: "default" | "landscape" | "application-form";
+  size?: "small" | "large";
+  landscape?: boolean;
 };
 
 export default OpenMorningsCard;
