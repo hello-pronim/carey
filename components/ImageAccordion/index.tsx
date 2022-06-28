78e27f6f2ea1ad5ed1e4c42089fb02960771daf9
leonace924 <meobius123@gmail.com>
2022-06-28 14:33:44 -0400

    feat: add image accordion component

---
+++ components/ImageAccordion/index.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Text } from "@components/common";
+import { AccordionWrapper, AccordionItem, AccordionContent } from "./styles";
+
+const ImageAccordion = ({ accordionData, ...props }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleMouseOver = (index: any) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <>
+      <AccordionWrapper>
+        {accordionData?.map((item, index) => {
+          const { title, subtitle, campusAccordionImage } = item;
+          const xPoint = campusAccordionImage?.[0]?.focalPoint?.[0] * 100 + "%";
+          const yPoint = campusAccordionImage?.[0]?.focalPoint?.[1] * 100 + "%";
+
+          return (
+            <AccordionItem
+              key={title}
+              css={{
+                backgroundImage: `url(${campusAccordionImage?.[0]?.url})`,
+                backgroundPosition: `${xPoint} ${yPoint}`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                "@min768": {
+                  flex: `${activeIndex === index ? "2.5" : "1"}`,
+                },
+              }}
+              onMouseOver={() => handleMouseOver(index)}
+            >
+              <AccordionContent>
+                {title && (
+                  <Text
+                    variant="Heading-Large"
+                    css={{
+                      color: "$white",
+                    }}
+                  >
+                    {title}
+                  </Text>
+                )}
+                {subtitle && (
+                  <Text
+                    css={{
+                      color: "$white",
+                      fontWeight: "400",
+                      "@min1024": {
+                        fontWeight: "600",
+                      },
+                    }}
+                  >
+                    {title}
+                  </Text>
+                )}
+              </AccordionContent>
+            </AccordionItem>
+          );
+        })}
+      </AccordionWrapper>
+    </>
+  );
+};
+
+export default ImageAccordion;
