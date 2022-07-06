d90ba89fc3724179b1509e10efd69b400b277308
leonace924 <meobius123@gmail.com>
2022-07-06 09:50:10 -0400

    feat: add image accordion to why carey page

--- components/pages/whyCarey/index.tsx
+++ components/pages/whyCarey/index.tsx
@@ -1,9 +1,24 @@
-import Hero from "@components/Hero";
 import React from "react";
+import { Container, PreFooter } from "@components/common";
+import Hero from "@components/Hero";
+import ImageAccordion from "@components/ImageAccordion";
+import { FullGridContainer } from "./styles";
 
 const WhyCarey = ({ pageData, applyNow }) => {
   return (
-    <Hero type="display" props={pageData?.whyCareyBanner} applyNow={applyNow} />
+    <>
+      <Hero
+        type="display"
+        props={pageData?.whyCareyBanner}
+        applyNow={applyNow}
+      />
+      <Container type="grid">
+        <FullGridContainer>
+          <ImageAccordion accordionData={pageData?.campuses} />
+        </FullGridContainer>
+      </Container>
+      <PreFooter />
+    </>
   );
 };
 
---
+++ components/pages/whyCarey/styles.tsx
@@ -0,0 +1,9 @@
+import { styled } from "@styles/stitches";
+
+const FullGridContainer = styled("div", {
+  gridColumn: "1 / span 12",
+});
+
+const Example = styled("div", {});
+
+export { Example, FullGridContainer };
