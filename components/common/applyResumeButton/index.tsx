00f89b5fb1fe6438f386e1244f3f5cb376b73eb9
Yalamber Subba <yalamber@mundhum.com>
2022-07-07 00:29:56 +0545

    popup width fixed

--- components/common/applyResumeButton/index.tsx
+++ components/common/applyResumeButton/index.tsx
@@ -39,9 +39,9 @@ const StyledContent = styled(AlertDialogPrimitive.Content, {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: "90vw",
-  maxWidth: "850px",
+  maxWidth: "650px",
   maxHeight: "85vh",
-  padding: 80,
+  padding: "50px 80px",
   zIndex: 9999,
   "@media (prefers-reduced-motion: no-preference)": {
     animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1) forwards`,
